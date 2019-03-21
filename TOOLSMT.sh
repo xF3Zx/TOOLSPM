9290af28c0db75147891ec4e06022575299ebb4e
xF3Zx <48673228+xF3Zx@users.noreply.github.com>
2019-03-21 08:15:41 +0700

    Add files via upload

---
+++ TOOLSMT.sh
@@ -0,0 +1,131 @@
+#!/system/xbin/bash
+#MauNgapainGblk?
+#DiRecodeGakBakalJadiinLuMastah
+#DiGantiAuthorGakBakalJadiinLuMaster
+#MikirMakeOtakGblk
+#KontolKaloMauReedit/Recode
+#Pm Gua Bangsat
+clear
+cd module
+python2 please.py
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+sleep 1
+echo $red
+cat asw.txt
+echo ""
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo "\033[32;1mAuthor : F3Z"
+echo "\033[37;1mFeat: F3R"
+echo "\033[35;1mJenis Tools: Tools SPAM"
+echo "\033[35;1mTeam : BOJONEGORO CYBER SECURITY"
+echo "\033[33;1mKontak Me : 085738995966"
+echo "\033[31;1mTahun Rilis : 27 Maret 2019"
+echo "\033[36;1mTULUNGAGUNG DARK CYBER"
+echo "\033[34;1m[$]\033[36;1m============================\033[34;1m[$]"
+echo ""
+echo "\033[32;1mSilahkan Pilih Tools Yang mau Kamu pake:)?:"
+echo "\033[36;1m"
+echo "[===============================================[>"
+echo $green "1.> Spam Sms Tokopedia"
+echo "[===============================================]>"
+echo $purple "2.> Spam Sms Telkomnyet"
+echo "[===============================================]>"
+echo $cyan "3.> Spam Sms Matahari Mall"
+echo "[===============================================]>"
+echo $red "4.> Spam Sms HooqTV"
+echo "[===============================================]>"
+echo $white "5.> Spam Sms KFC"
+echo "[===============================================]>"
+echo $green "6.> Spam Sms PHD"
+echo "[===============================================]>"
+echo $purple "7.> Spam Sms Whiskas"
+echo "[===============================================]>"
+echo $cyan "8.> Spam Sms ZiPAY"
+echo "[===============================================]>"
+echo $yellow "0.> keluar"
+echo "[+]===============================================[+]"
+echo "\033[32;1m"
+read -p "root@K0NT00Ls-F3Z~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "F3Z" | lolcat
+php tp.php
+fi
+
+if
+[ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet "F3Z"
+php t.php
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+figlet "F3Z"
+php mataharimall.php
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+toilet "F3Z"
+php hooq.php
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+toilet -f slant --gay "F3Z"
+php kfc.php
+fi
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+toilet -f mono12 -F gay "F3Z"
+php phd.php
+fi
+
+if [ $bro = 7 ] || [ $bro = 7 ]
+then
+clear
+toilet "F3Z"
+php whiskas.php
+fi
+
+if [ $bro = 8 ] || [ $bro = 8 ]
+then
+clear
+figlet "F3Z"
+php zipay.php
+fi
+
+if [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mBOJONEGORO CYBER SECURITY"
+sleep 1
+echo "\033[33;1mTULUNGAGUNG DARK CYBER"
+sleep 1
+echo " We Not Friends"
+sleep 1
+echo "We Are Family"
+sleep 1
+echo "Hacking Is Not Criminal;)"
+sleep 1
+echo "Ketika Sebuah Hayalan Tidak tercapai"
+sleep 1
+echo "Maka Terus lah BerJuang Dan Berusaha:)"
+sleep 1
+echo "\033[32;1mKarna Suatu Hari Nanti Kamu akan Mendapatkannya:)"
+sleep 1
+exit
+fi
